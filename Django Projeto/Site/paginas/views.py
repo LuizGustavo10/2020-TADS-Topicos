51acbd22e3a151ac1927531f5239acab687c4ace
Gu3001 <gustavo180g@gmail.com>
2020-04-09 08:25:43 -0300

    projeto django definitivo adicionado

---
+++ Django Projeto/Site/paginas/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    # template_name = "paginas/index.html"
+    template_name = "paginas/modelo.html"
+
+class SobreView(TemplateView):
+    template_name = 'paginas/sobre.html'
