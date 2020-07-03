614b1bfb17cb993517604ecf0f82efc8686c4801
Gu3001 <gustavo180g@gmail.com>
2020-07-03 08:37:07 -0300

    segunda parte

--- Django Projeto/Site/paginas/urls.py
+++ Django Projeto/Site/paginas/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import IndexView, SobreView
+from .views import IndexView, SobreView, CadastrarFichaView, CadastrarFuncionarioView, CadastrarPessoaView
 
 urlpatterns = [
     # path('endereço/', MinhaView.as_view(), name='nome-da-url'),
     path('', IndexView.as_view(), name='index'),
     path('sobre/', SobreView.as_view(), name='sobre'),
+    path('cadastrarPessoa/', CadastrarPessoaView.as_view(), name='cadastrarPessoa'),
+    path('cadastrarFuncionario/', CadastrarFuncionarioView.as_view(), name='cadastrarFuncionario'),
+    path('ficha/', CadastrarFichaView.as_view(), name='ficha'),
 ]
--- Django Projeto/Site/paginas/views.py
+++ Django Projeto/Site/paginas/views.py
@@ -3,6 +3,17 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     # template_name = "paginas/index.html"
     template_name = "paginas/modelo.html"
+    
+class CadastrarPessoaView(TemplateView):
+    template_name = 'paginas/cadastrarPessoa.html'
+
+    
+class CadastrarFuncionarioView(TemplateView):
+    template_name = 'paginas/cadastrarFuncionario.html'
+
+class CadastrarFichaView(TemplateView):
+    template_name = 'paginas/ficha.html'
 
 class SobreView(TemplateView):
     template_name = 'paginas/sobre.html'
+
