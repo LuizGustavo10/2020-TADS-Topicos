d666f151e83828f2820354807707dcc6258ba580
Gu3001 <gustavo180g@gmail.com>
2021-01-06 16:58:45 -0300

    aula 25 e 26, cadastro de função, e não repetir email

--- Django Projeto/Site/usuarios/views.py
+++ Django Projeto/Site/usuarios/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from .forms import UsuarioForm
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
@@ -13,6 +14,13 @@ class UsuarioCreate(CreateView):
     success_url = reverse_lazy('login')
     # model = User
     # fields = ['username','email','password']
+    def form_valid(self, form):
+        grupo = get_object_or_404(Group, name="Funcionario")
+        url = super().form_valid(form)
+        
+        self.object.groups.add(grupo)
+        self.object.save()
+        return url;
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
